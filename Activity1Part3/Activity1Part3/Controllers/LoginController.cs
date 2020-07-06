afaba9e315c66ce0b3901068440138b2ead9c9e0
patdalcia <49659510+patdalcia@users.noreply.github.com>
2020-07-05 22:06:53 -0700

    Initial commit

---
+++ Activity1Part3/Activity1Part3/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            SecurityService s = new SecurityService();
+            bool flag = s.Authenticate(model);
+            if(flag == true)
+            {
+                return View("LoginPassed");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
---
+++ Activity1Part3/Activity1Part3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,36 @@
+﻿using Activity1Part3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Data
+{
+    public class SecurityDAO
+    {
+        public bool FindByUser(UserModel user)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            connection.Open();
+
+            string sql = "SELECT * FROM dbo.Users";
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
